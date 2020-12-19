6d25e6887c5e7fe8b415785914b6145027c3ad1e
caraorts <59981874+caraorts@users.noreply.github.com>
2020-12-19 19:01:28 +0100

    Add files via upload

--- Ejercicio 3/ejercicio3.m
+++ Ejercicio 3/ejercicio3.m
@@ -1,8 +1,7 @@
 %% Ejercicio 3
 clear;
 close all
-clc
-
+ 
 global vyt Y T NAR M H N vnames
 %% Cargamos los datos %%
 data = xlsread('data','data', 'B2:G1045');
@@ -43,8 +42,8 @@ for i=1:size(r,2)
     paramopt(i,4) = thtropt(2);
     paramopt(i,5) = -exp(thtropt(5));
 
-    % Con los parámetros óptimos, calculamos volatilidad condicional y 
-    % valor de fº verosimilitud
+    % Con los parámetros óptimos, calculamos volatilidad condicional y valor de
+    % fº verosimilitud
     [lfv(:,i), vsigma2(:,i)] = elfv(thtropt);
 end 
 vsigma2_ib35 = vsigma2(:,1); vsigma2_sab = vsigma2(:,2);
@@ -76,39 +75,38 @@ plot([vsigma2t(:,6) vsigma2(:,6)]);
 
 Y = [log(vsigma2)];
 
-% Contratamos un test sobre razón de verosimilitud para determinar el orden
-% óptimo del VAR.
+% Contratamos un test sobre razón de verosimilitud para determinar el orden óptimo 
+% del VAR.
 sims = 1;
 maxlag = 16;
 minlag = 1;
 lrratio(Y,maxlag,minlag,sims);
 
 % Contrastamos si elegimos un retardo (p0) o 14 retardos (p1)
-% Contrastar p1 retardos contra p0
+%Contrastar p1 retardos contra p0
 display('_______________________________________________________________');
 display('Contraste 14 retardos versus 1');
 likratio(Y,14,1,0.05,1)
 display('_______________________________________________________________');
 
-% Rechazamos H_0: nos quedamos con un VAR(1).
-
+% LR > VC --> nos quedamos con un VAR(1).
 %%  Estimamos el modelo VAR %%
 T = length(Y);  %tamaño muestral
-NAR = 1;  % Número de retardos del VAR
-M = 50;   % Número de retardos del VMA(inf)
-H = 12;   % Horizonte de predicción para la descomposición de la varianza
+NAR = 1;  % número de retardos del VAR
+M = 50;   % número de retardos del VMA(inf)
+H = 20 %20 % horizonte de predicción para la descomposición de la varianza
 N = size(Y,2);
 
 [results]=fvar(Y,NAR);
-MPHI=mphi(results,NAR,N);
 
-% Matriz de varianzas y covarianzas
+MPHI=mphi(results,NAR,N);
+%Calculando la matriz de varianzas y covarianzas
 for i = 1:N
     err(:,i) = results(i).resid;
 end
 Omega = err'*err/rows(err);
 
-% Predicciones del VAR
+% Calculando las predicciones del VAR
 for i = 1:N
     yhat(:,i) = results(i).yhat;
 end
@@ -117,20 +115,28 @@ vnames =  ['IBEX           ',
            'SABADELL       ',    
            'BBVA           ',
            'SANTANDER      ',
-           'INDICE_BANCARIO',
+           'INDICE BANCARIO',
            'BANKIA         '];
 names={'IBEX' 'SABADELL' 'BBVA' 'SANTANDER' 'INDICE BANCARIO' 'BANKIA'};
 
 plt_var(results,vnames); 
 prt_var(results,vnames);
 
-% Ninguna es significativa salvo:
-%
-% - SAN: IBEX y BKA
-% - IB : IBEX y BKA
+% En el test de Grenger obtenemos lo siguiente:
+% -- En la regresión de BBVA, parece que todas las variables contribuyen a
+% explicar la vol de BBVA menos el indice bancario FTS100
+% -- En la regresión de Sabadell, parece que todas las variables, con sus
+% retrasos, explican la vol de Sabadell (rechazamos todas las H0 a un 1%).
+% -- En la regresión de Santander, Sabdell no explica a un 5% la vol de
+% Santander. Mientras que el Indice Bancaria explica la volatilidad del
+% SAN a partir del 2.4%.
+% -- En la regresión VAR de IBEX-35, de nuevo todas las variables, con sus
+% retrasos, parecen explicar la vol del IBEX-35 (rechazamos H0)
+% -- En la regresión sobre el Indice Bancario lo mismo que para IBEX35 y
+% SAbadell
 
-%% Análisis de los residuos
-figure;
+% Análisis de los residuos
+figure(3);
 subplot(4,3,1);
 autocorr(err(:,1),15);
 title('ACF: IBEX' );
@@ -168,8 +174,8 @@ subplot(4,3,12);
 parcorr(err(:,6),15);
 title('PACF: BANKIA');
 
-%% Analizamos conectividad mediante una indentificación generalizada.
 
+  %% Analizamos conectividad mediante una indentificación generalizada.
 [MFRI,MMA]=fri(results, MPHI, NAR, N, M, P, vnames);
 DeltaC = zeros(N,N);
 for i = 1:N
@@ -197,9 +203,24 @@ end
 disp('_____________________________________________________');
 disp('Horizonte de previsión en la descomposición de la varianza');
 H
+Deltat = Deltat*100;
 disp('_____________________________________________________');
-Deltat=Deltat.*100;
+disp('Connectedness Table');
+disp('Cada elemento (i,j) nos dice en qué porcentaje el shock de la variable j explica la varianza del error de predicción de la variable i');
+Deltat
 
+% MDeltat_b=deltat_boot(err,yhat,100);
+% Deltat=prctile(MDeltat_b,50,3);
+% disp('Connectedness Table');
+% disp('Cada elemento (i,j) nos dice en qué porcentaje el shock de la variable j explica la varianza del error de predicción de la variable i');
+% prctile(MDeltat_b,50,3)
+% %
+% disp('Connectedness Table: percentil 97.5');
+% prctile(MDeltat_b,97.5,3)
+% disp('Connectedness Table: percentil 2.5');
+% prctile(MDeltat_b,2.5,3)
+%
+%
 disp('_____________________________________________________');
 disp('Net pairwise directional connectedness measures');
 disp('Cada elemento (i,j) mide el efecto neto: en qué porcentaje i explica a j menos en qué porcentaje j explica a i');
@@ -213,11 +234,14 @@ Deltatnet
 disp('_____________________________________________________');
 disp('Total directional Connectedness from others to i');
 disp('Mide qué porcentaje de la varianza del error de previsión de i es explicado por el resto de las variables del VAR');
-sum(Deltat,2)-diag(Deltat)
+tot_i = sum(Deltat,2)-diag(Deltat)
+tabla2 = table(tot_i)
 disp('_____________________________________________________');
 disp('Total directional Connectedness from j to others');
 disp('Mide qué porcentaje de la varianza del error de previsión de todas las variables (excepto j) es explicado por j');
-sum(Deltat,1)-diag(Deltat)'
+tot_j = sum(Deltat,1)-diag(Deltat)'
+tabla3 = table(tot_j)
+
 disp('_____________________________________________________');
 disp('Net total directional connectedness');
 disp('Mide el porcentaje que explica i de los otros - el porcentaje de i explicado por otros');
@@ -234,7 +258,6 @@ end
 disp('Total connectedness');
 SS=SS/N
 disp('_____________________________________________________');
-
 %% HACEMOS GRÁFICO
 ss=[];
 tt=[];
@@ -252,6 +275,11 @@ weights1 = round(weights/max(weights)*100);LWidth=4/100*weights1;
 G = digraph(ss,tt,weights1,names);
 %VNodeCData=[0 0.5 1 1.5 2 2.5 3 3.5 4 4.5];
 VNodeCData=[0 0.5 1 1.5 2 2.5];
-plot(G,'Layout','force','EdgeLabel',G.Edges.Weight,'LineWidth',LWidth,...
-    'ArrowSize',10,'markersize',20,'NodeCData',VNodeCData,'EdgeColor','k')
+plot(G,'Layout','force','EdgeLabel',G.Edges.Weight,'LineWidth',LWidth,'ArrowSize',10,'markersize',20,'NodeCData',VNodeCData,'EdgeColor','k')
 axis equal off
+%plot(G,'Layout','layered','EdgeLabel',G.Edges.Weight,'LineWidth',LWidth,'ArrowSize',10,'markersize',20)
+axis equal off
+
+
+
+
